69f4d4b5510ff1a78bcee9cf7a1b3a44721ed8fa
kübra <kubra3.y@gmail.com>
2026-02-01 13:04:43 +0300

    added entrance and exit portals to scenes

--- GGJ2026/Assets/Scripts/LevelExit.cs
+++ GGJ2026/Assets/Scripts/LevelExit.cs
@@ -17,9 +17,17 @@ public class LevelExit : MonoBehaviour
 
     IEnumerator LoadNextLevel()
     {
+        //currenti al eğer 3se entrancea at 
         yield return new WaitForSecondsRealtime(1f);
-        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
         
+        //if it's the last exit
+        int currenstSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currenstSceneIndex == 3)
+        {
+            SceneManager.LoadScene(0);
+        }
+        
+        int nextSceneIndex = currenstSceneIndex + 1;
         
         if (nextSceneIndex != SceneManager.sceneCountInBuildSettings)
         {
